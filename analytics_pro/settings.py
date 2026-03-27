fba7aa0f1acf4349b1ebe3c0dcc897c8719b063b
Poonam Shinde <poonamshinde1628@gmail.com>
2026-03-27 09:31:45 +0530

    Auto migrate on render

--- analytics_pro/settings.py
+++ analytics_pro/settings.py
@@ -137,3 +137,7 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+if os.environ.get('RENDER'):
+    from django.core.management import call_command
+    call_command('migrate')
